b4ef1bb2e1d7a9c20928755c246074a844c25d60
JHTA <JHTA@JHTA-PC>
2018-10-05 16:39:09 +0900

    admin contoller

--- final_1802/src/bean/AdminController.java
+++ final_1802/src/bean/AdminController.java
@@ -7,22 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class TablesController {
-		TablesDao dao;
+public class AdminController {
+		AdminDao dao;
 
 		
-		public TablesController(TablesDao dao) {
+		public AdminController(AdminDao dao) {
 			this.dao = dao;
 
 		}
 		
-		@RequestMapping(value="/list.tbl")
+		@RequestMapping(value="/table_status.adm")
 		public ModelAndView menu_list(String str) {
 			ModelAndView mv = new ModelAndView();
 			
 			
 			
-			mv.setViewName("admin_index");
+			mv.setViewName("table_status");
 			return mv;
 		}
 }
