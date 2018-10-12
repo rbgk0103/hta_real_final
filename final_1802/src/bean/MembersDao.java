3b2c77223cae77e9a5a75153090f1785a905c037
JHTA <JHTA@JHTA-PC>
2018-10-12 15:54:07 +0900

    kms-members

--- final_1802/src/bean/MembersDao.java
+++ final_1802/src/bean/MembersDao.java
@@ -10,15 +10,13 @@ import org.apache.ibatis.session.SqlSession;
 
 public class MembersDao {
 	
-	SignCodeVo svo;
 	MembersVo mvo;
 	PageCompute page;
 	
 	SqlSession s;
 	
-	public MembersDao(SignCodeVo svo, MembersVo mvo, PageCompute page) {
+	public MembersDao(MembersVo mvo, PageCompute page) {
 		s = MbrFactory.getFactory().openSession();
-		this.svo = svo;
 		this.mvo = mvo;
 		this.page = page;
 	}
