4be7d3bcfd9cc4c29912efdd276e64c2f1056672
iyabong <iyabong@gmail.com>
2018-10-15 10:49:19 +0900

    게임 사용자
    테이블 vs 테이블
    웹소켓 테스트

---
+++ final_1802/src/bean/WebSocketTvTServer.java
@@ -0,0 +1,42 @@
+package bean;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/gameBroadcasting")
+public class WebSocketTvTServer {
+
+	/* 필드 */
+	private static Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
+	
+	/* 메서드들*/
+	@OnMessage
+	public void onMessage(String msg, Session session) throws Exception {
+		System.out.println("receive message : " + msg);
+		for (Session s : clients) {
+			s.getBasicRemote().sendText(msg);
+		}
+	}
+
+	@OnOpen
+	public void onOpen(Session s) {
+		System.out.println("session open : " + s);
+		clients.add(s);
+		System.out.println("현재 접속한 사용자 수: " + clients.size());
+		
+	}
+
+	@OnClose
+	public void onClose(Session s) {
+		System.out.println("session close : " + s);
+		clients.remove(s);
+	}
+	
+}	// End of Class
